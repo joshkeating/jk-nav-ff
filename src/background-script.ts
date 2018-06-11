ad84c81a4abdfb568922e4f18a572c4e3b32107a
joshkeating <josh.keating7@gmail.com>
2018-06-11 15:48:24 -0700

    pass those messages

--- src/background-script.ts
+++ src/background-script.ts
@@ -48,6 +48,7 @@ allSites.push(new Page('^https?://(www\.)?google\.([a-z\.]+)\/(?!reader\/).*$', 
 allSites.push(new Page('https?://(www\.)?reddit\.com\/.*', '#siteTable div.entry a.title', true));
 allSites.push(new Page('https?://arstechnica\.com\/.*', 'h2 > a', true));
 
+
 // determines if given url should be acted on
 function checkSiteValid(url: string): string {
     for (let index = 0; index < allSites.length; index++) {
@@ -60,39 +61,47 @@ function checkSiteValid(url: string): string {
     return null;
 }
 
-// function getSite(url: string): number {
-//     for (let index = 0; index < allSites.length; index++) {
-//         const element: Page = allSites[index];
-//         let regex: RegExp = new RegExp(element.getUrl(), 'i');
-//         if (regex.test(url)) {
-//             return index;
-//         } 
-//     }
-// }
+function handleMessage(message, sender, response) {
+    // return browser.bookmarks.search({
+    //   url: message.url
+    // }).then(function(results) {
+    //   return results.length > 0;
+    // });
 
-// Listener keeps the event page open until not needed
-browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    
-    if (checkSiteValid(request.url)) {
+    if (checkSiteValid(message.url)) {
 
-        // show page icon if site is valid
-        // chrome.pageAction.show(sender.tab.id);
-
-        let responseString: string = checkSiteValid(request.url);
-        sendResponse({pattern: responseString});
-
-        // // TODO: handle toggle of the site
-        // send url over to popup
-        // chrome.runtime.sendMessage({thisSite: allSites[getSite(request.url)]});
-        // if (request.cmd == 'toggle') {
-        //     allSites[getSite(request.url)].setEnabled(request.enabled);
-        //     chrome.runtime.sendMessage({temp: allSites[getSite(request.url)].getEnabled()});
-        //     chrome.tabs.executeScript(request.url, {code: 'location.reload()'}, function() {});
-        // } else {
-        //     let responseString: string = checkSiteValid(request.url);
-        //     sendResponse({pattern: responseString});
-        // }
-        
+        let responseString: string = checkSiteValid(message.url);
+        response({pattern: responseString});
     }
     return true;
-});
+  }
+
+browser.runtime.onMessage.addListener(handleMessage)
+
+
+// Listener keeps the event page open until not needed
+// browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    
+//     if (checkSiteValid(request.url)) {
+
+//         // show page icon if site is valid
+//         // chrome.pageAction.show(sender.tab.id);
+
+//         let responseString: string = checkSiteValid(request.url);
+//         sendResponse({pattern: responseString});
+
+//         // // TODO: handle toggle of the site
+//         // send url over to popup
+//         // chrome.runtime.sendMessage({thisSite: allSites[getSite(request.url)]});
+//         // if (request.cmd == 'toggle') {
+//         //     allSites[getSite(request.url)].setEnabled(request.enabled);
+//         //     chrome.runtime.sendMessage({temp: allSites[getSite(request.url)].getEnabled()});
+//         //     chrome.tabs.executeScript(request.url, {code: 'location.reload()'}, function() {});
+//         // } else {
+//         //     let responseString: string = checkSiteValid(request.url);
+//         //     sendResponse({pattern: responseString});
+//         // }
+        
+//     }
+//     return true;
+// });
--- src/contentScript.ts
+++ src/contentScript.ts
@@ -9,46 +9,52 @@ let color: string;
 
 function sendURL() {
 
-    let pattern: string;
-    browser.runtime.sendMessage({url: location.href}, function(response) {
-        // the response that this recives should either be the data associated
-        // with this site (it exists) or undefined/disabled.
-        if (!response) {
-            return;
-        }
+    
+    function handleResponse(pattern: string) {
+        if (pattern) {
+
+            $(document).ready(function() {
+
+                let currentIndex: number = 0;
         
-        // wait till DOM is fully loaded to execute
-        $(document).ready(function() {
-            pattern = response.pattern;
-            let currentIndex: number = 0;
+                let currentNode: HTMLElement;
+        
+                let allLinks = document.querySelectorAll(pattern) as NodeListOf<HTMLElement>;
+                currentNode = allLinks[currentIndex];
+                currentNode.style.backgroundColor = color;
+                currentNode.focus();
+        
+                document.addEventListener("keypress", function onEvent(event) {
+                    let previousIndex: number = currentIndex;
+                    if (event.key === "j" && currentIndex < allLinks.length - 1) {
     
-            let currentNode: HTMLElement;
+                        currentNode = allLinks[currentIndex+=1];
+                        allLinks[previousIndex].style.backgroundColor = "inherit";
+                        currentNode.style.backgroundColor = color;
+                        currentNode.focus();
+                    }
+                    else if (event.key === "k" && currentIndex > 0) {
+                        currentNode = allLinks[currentIndex-=1];
+                        allLinks[previousIndex].style.backgroundColor = "inherit";
+                        currentNode.style.backgroundColor = color;
+                        currentNode.focus();
+                    }
+                });
     
-            let allLinks = document.querySelectorAll(pattern) as NodeListOf<HTMLElement>;
-            currentNode = allLinks[currentIndex];
-            currentNode.style.backgroundColor = color;
-            currentNode.focus();
-    
-            document.addEventListener("keypress", function onEvent(event) {
-                let previousIndex: number = currentIndex;
-                if (event.key === "j" && currentIndex < allLinks.length - 1) {
+             });
+          
+        } 
 
-                    currentNode = allLinks[currentIndex+=1];
-                    allLinks[previousIndex].style.backgroundColor = "inherit";
-                    currentNode.style.backgroundColor = color;
-                    currentNode.focus();
-                }
-                else if (event.key === "k" && currentIndex > 0) {
-                    currentNode = allLinks[currentIndex-=1];
-                    allLinks[previousIndex].style.backgroundColor = "inherit";
-                    currentNode.style.backgroundColor = color;
-                    currentNode.focus();
-                }
-            });
+        return;
+      }
+      
+    let pattern: string;
+    let url: string = window.location.href;
 
-         });
+    browser.runtime.sendMessage({
+        url: window.location.href
+    }).then(handleResponse)
 
-      });
 }
 
 sendURL();
