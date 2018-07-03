7be3d557c5bd19be503e040b860a4578a6c7d3bb
joshkeating <josh.keating7@gmail.com>
2018-07-02 19:20:35 -0700

    cleaned up popup code, closes properly

--- src/options.ts
+++ src/options.ts
@@ -1,30 +1,25 @@
 
- 
-document.addEventListener("click", (e) => {
+ function handleClicks(): void {
 
-    // do this silly dance to tell TS that this will be an element
-    let temp = (<Element>e.target).getAttribute("value");
+    document.addEventListener("click", (e) => {
 
-    console.log(temp);
-    // browser.storage.local.set({temp});
-    browser.storage.local.set({currentColor: temp}); 
+        // do this silly dance to tell TS that this will be an element
+        let temp = (<Element>e.target).getAttribute("value");
+    
+        console.log(temp);
+        // browser.storage.local.set({temp});
+        browser.storage.local.set({currentColor: temp});
+    
+        window.close();
+            
+        // reloading is broken 
+        // location.reload();
+        // window.location.reload(true);
+    
+    });
+ }
 
-    // let url = beastNameToURL(e.target.textContent);
-    // let activatedColor = e.target.value;
+ handleClicks();
 
-    // let option: Element = document.querySelector(temp);
-    // console.log(option);
-
-});
-
-function getColor() {
-    let selectedColor = document.getElementById("popup-content").getAttribute("value");
-    return selectedColor;
-}
-
-function setColor() {
-    let currentColor: string = getColor();
-    browser.storage.local.set({currentColor})
-}
 
 
